d49cf614e4eb52b46a54a3b94609cf487fbc49c2
Liam Huynh <liam@crowdhubapps.com>
2022-12-07 01:10:24 -0600

    working on the herobanner and integrated with sanity

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Product, FooterBanner, HeroBanner} from '../components'
 
-const index = () => {
+const Home = () => {
   return (
-    <div>index</div>
+    <>
+      <HeroBanner/>
+
+      <div className='products-heading'>
+        <h2>Best Selling Products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+
+      <div className='products-container'>
+        {['product 1', 'product 2'].map(( product ) => product)}
+      </div>
+      
+      <FooterBanner/>
+    </>
   )
 }
 
-export default index
+export default Home
